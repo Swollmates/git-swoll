d724bdbaa2cfa15230ec5770aef9eb81592fed2c
Michael Roque <mikeroque1@gmail.com>
2016-12-30 15:34:56 -0700

    created user seed

---
+++ seeds/01_users.js
@@ -0,0 +1,36 @@
+exports.seed = function(knex, Promise) {
+    // Deletes ALL existing entries
+    return knex.raw('DELETE FROM "user"; ALTER SEQUENCE user_id_seq RESTART WITH 4;')
+        .then(function() {
+            return Promise.all([
+                // Inserts seed entries
+                knex('user').insert({
+                    id: 1,
+                    first_name: 'Aaron',
+                    last_name: 'Goodman',
+                    email: 'amgoodman5@gmail.com',
+                    password: 'aaron',
+                    joined: new Date(),
+                    is_active: true
+                }),
+                knex('user').insert({
+                    id: 2,
+                    first_name: 'Michael',
+                    last_name: 'Roque',
+                    email: 'mycroquet@gmail.com',
+                    password: 'michael',
+                    joined: new Date(),
+                    is_active: true
+                }),
+                knex('user').insert({
+                    id: 3,
+                    first_name: 'Matt',
+                    last_name: 'Winzer',
+                    email: 'matt.winzer@gmail.com',
+                    password: 'matt',
+                    joined: new Date(),
+                    is_active: true
+                })
+            ]);
+        });
+};
